81522f07ba43238954759a65ea0a5d6244817b09
Anna Harrison <anna@Macminis-Mac-mini.local>
2017-10-15 16:24:53 -0400

    First commit

---
+++ StitchCounter/StitchCounter/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StitchCounter
+//
+//  Created by Anna Harrison on 10/10/17.
+//  Copyright © 2017 Anna Harrison. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
